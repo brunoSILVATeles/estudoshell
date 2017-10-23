628351ace073468fc188e4160c2a146d35698f65
bruno <bruno77silvaa@gmail.com>
2017-10-23 12:20:27 -0200

    primeiros comandos em shell

---
+++ calculos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Programa para calcular dois numeros e fazer as quatro aperacoes
+clear
+echo "-------------------------------------------------------"
+echo "-                                                     -"
+echo "-             programa de calculos: 4 operacoes       -"
+echo "-                                                     _"
+echo "--------------------------------------------------------"
+echo ""
+echo "digite outro numero"
+read n2
+echo "digite outro numero"
+read n2
+echo "o resultado da soma eh: $((n1+n2))"
+echo "o resultado da subtracao eh: $((n1-n2))"
+echo "o resultado da maltiplicacao eh: $((n1*n2))"
+echo "o resultado da divisao eh: $((n1/n2))"
+echo "o calculo foi realizado:`./tempo.sh`"
+                                                     
+
---
+++ segundo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+clear
+echo "Entre com seu de usuario"
+read usuario
+echo "Entre com a sua senha"
+read senha
+echo "o usuario:$usuario logout: `./tempo.sh`"
